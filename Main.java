e5c64347b5c7f708c4ce606e25407c00db9bd24f
JackInferno <131201139+JackInferno@users.noreply.github.com>
2025-06-05 15:33:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
 
 public class Main {
-    static final int ARRAY_SIZE = 10000000;
+    static final int ARRAY_SIZE = 10_000_000;
     static int[] array = new int[ARRAY_SIZE];
 
     static int min = Integer.MAX_VALUE;
     static int minIndex = -1;
 
-    static final Object lock = new Object();
+    static final Object resultLock = new Object();
+    static final Object threadCountLock = new Object();
+    static int threadsDone = 0;
 
     public static void main(String[] args) {
         generateArray();
@@ -16,40 +17,31 @@ public class Main {
         int numThreads = 4;
         int chunkSize = ARRAY_SIZE / numThreads;
 
-        CountDownLatch latch = new CountDownLatch(numThreads);
-
         for (int i = 0; i < numThreads; i++) {
             int start = i * chunkSize;
             int end = (i == numThreads - 1) ? ARRAY_SIZE : start + chunkSize;
 
             Thread thread = new Thread(() -> {
-                int localMin = Integer.MAX_VALUE;
-                int localIndex = -1;
+                findLocalMin(start, end);
 
-                for (int j = start; j < end; j++) {
-                    if (array[j] < localMin) {
-                        localMin = array[j];
-                        localIndex = j;
-                    }
+                synchronized (threadCountLock) {
+                    threadsDone++;
+                    threadCountLock.notify();
                 }
-
-                synchronized (lock) {
-                    if (localMin < min) {
-                        min = localMin;
-                        minIndex = localIndex;
-                    }
-                }
-
-                latch.countDown();
             });
 
             thread.start();
         }
 
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        synchronized (threadCountLock) {
+            while (threadsDone < numThreads) {
+                try {
+                    threadCountLock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         System.out.println("Min value: " + min + ", at index: " + minIndex);
@@ -58,9 +50,27 @@ public class Main {
     static void generateArray() {
         Random rand = new Random();
         for (int i = 0; i < ARRAY_SIZE; i++) {
-            array[i] = rand.nextInt(10_000); //
+            array[i] = rand.nextInt(10_000);
         }
-        int negIndex = rand.nextInt(ARRAY_SIZE);
-        array[negIndex] = -rand.nextInt(1000);
+        array[rand.nextInt(ARRAY_SIZE)] = -rand.nextInt(1000);
+    }
+
+    static void findLocalMin(int start, int end) {
+        int localMin = Integer.MAX_VALUE;
+        int localIndex = -1;
+
+        for (int i = start; i < end; i++) {
+            if (array[i] < localMin) {
+                localMin = array[i];
+                localIndex = i;
+            }
+        }
+
+        synchronized (resultLock) {
+            if (localMin < min) {
+                min = localMin;
+                minIndex = localIndex;
+            }
+        }
     }
 }
